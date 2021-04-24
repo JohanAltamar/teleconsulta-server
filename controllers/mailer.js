9067bd740fa42ba55ed95cf56c6f962101d92280
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-24 16:16:47 -0500

    message without attachments added

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -3,11 +3,19 @@ const recomendaciones = require("../utils/recomendaciones");
 
 const sendProcess = async (req, res) => {
   try {
+<<<<<<< HEAD
     const files = await attachFiles2(req.files)
     await sendMail(JSON.parse(req.body.mailInfo), files);
     return res.json({ msg: 'Email enviado con éxito' })
   } catch (error) {
     console.log(error.code, ":", error.message)
+=======
+    const files = req.files ? await attachFiles2(req.files) : []
+    await sendMail(JSON.parse(req.body.mailInfo), files);
+    return res.json({ msg: 'Email enviado con éxito' })
+  } catch (error) {
+    console.log(error)
+>>>>>>> 83761a3 (message without attachments added)
     res.status(500).json({
       msg: "Algo salió mal, contacte al admin",
       code: error.code
